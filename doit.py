d7e6cbf2f4f2b971a4e2d628b7ded5de1822531e
Peter Richardson <github@zoomy.net>
2017-12-07 11:48:45 -0500

    update doit.py

--- doit.py
+++ doit.py
@@ -1,17 +1,26 @@
 import subprocess
 from datetime import datetime
-import os
+import os, sys
 
 startTime = datetime.now()
 
-# os.system('python collect-png.py --bounds 45.773097 -124.1536 46.299099 -123.358251 --zoom 14 --mapzen_api_key XXXXXX ./images/')
+if os.path.exists('./images/'):
+	os.system('rm -rf ./images/')
+
+os.makedirs('./images/')
+
+try:
+	os.system('python collect-png-parallel.py --bounds 40.6997802 -74.0193432 40.877619 -73.9104387 --zoom 13 --mapzen_api_key XXXXXXX ./images/')
+except:
+	print("Error")
+	sys.exit(0)
 
 if not os.path.exists('./images/converted'):
     os.makedirs('./images/converted')
 
 # os.system('for i in ./images/*.png; do echo $i; gdal_translate -ot UInt16 -of PNG -scale 0 8900 0 65536 -co worldfile=no --config GDAL_PAM_ENABLED NO $i ./images/converted/$(basename $i).png; done;')
 
-os.system('for i in ./images/*.png; do echo $i; python ../landgrab/convert-terrain.py -- $i; done;')
+os.system('for i in ./images/*.png; do echo $i; python ./convert-terrain.py -- $i; done;')
 
 os.system('/Applications/Blender.app/Contents/MacOS/blender --python dir_to_blender2.py -- ./images/converted 100')
 
